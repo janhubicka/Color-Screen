2f3730e06ad9c5ff6c581f8c4f5dbed00cc6795f
Jan Hubicka <hubicka@ucw.cz>
2022-10-16 14:28:55 +0200

    Maintainer mode.

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # first install MSYS2 packaging of mingw64 from https://www.mingw-w64.org/downloads/#msys2
 # then run this script
-pacman -S mingw-w64-x86_64-gcc mingw-w64-x86_64-gtk2 mingw-w64-x86_64-libtiff mingw-w64-x86_64-libjpeg-turbo git make diffutils automake autoconf mingw-w64-x86_64-pkg-config vim gdb
+pacman -S mingw-w64-x86_64-gcc mingw-w64-x86_64-gtk2 mingw-w64-x86_64-libtiff mingw-w64-x86_64-libjpeg-turbo git make diffutils automake autoconf mingw-w64-x86_64-pkg-config vim gdb libtool
 git clone https://github.com/janhubicka/Color-Screen.git ColorScreen
 mkdir ColorScreen-build
 cd ColorScreen-build/
