bb6808c7bba85a254cdfbf4fd13842cd2dca877a
Jan Hubicka <hubicka@ucw.cz>
2023-01-19 09:31:39 +0100

    Fix gamma.

--- src/libcolorscreen/scr-detect.C
+++ src/libcolorscreen/scr-detect.C
@@ -7,24 +7,36 @@ scr_detect::set_parameters (scr_detect_parameters param, luminosity_t gamma, int
 {
   m_param = param;
   lookup_table = render::get_lookup_table (gamma, maxval);
-  color_t red = (m_param.red.sgngamma (1 / gamma) - m_param.black.sgngamma (1 / gamma)).normalize ();
-  color_t green = (m_param.green.sgngamma (1 / gamma) - m_param.black.sgngamma (1 / gamma)).normalize ();
-  color_t blue = (m_param.blue.sgngamma (1 / gamma) - m_param.black.sgngamma (1 / gamma)).normalize ();
-  color_matrix t (1, 0, 0, m_param.black.sgngamma (1 / gamma).red,
-		  0, 1, 0, m_param.black.sgngamma (1 / gamma).green,
-		  0, 0, 1, m_param.black.sgngamma (1 / gamma).blue,
+  color_t black = m_param.black.sgngamma (gamma);
+  color_t red = m_param.red.sgngamma (gamma);
+  color_t green = m_param.green.sgngamma (gamma);
+  color_t blue = m_param.blue.sgngamma (gamma);
+  red = (red - black).normalize ();
+  green = (green - black).normalize ();
+  blue = (blue - black).normalize ();
+  color_matrix t (1, 0, 0, black.red,
+		  0, 1, 0, black.green,
+		  0, 0, 1, black.blue,
 		  0, 0, 0, 1);
   color_matrix m (red.red,   green.red,   blue.red,   0,
 		  red.green, green.green, blue.green, 0,
 		  red.blue,  green.blue,  blue.blue,  0,
 		  0, 0, 0, 1);
   //printf ("basis\n");
-  //m.print(stdout);
+  //t.print(stdout);
   t = t * m;
+  //t.print(stdout);
   //printf ("Forward color transform:\n");
   //t.print(stdout);
   //printf ("Backward color transform:\n");
   m_color_adjust = t.invert ();
+  //m_color_adjust.print(stdout);
+
+  //luminosity_t rr,gg,bb;
+  //m_color_adjust.apply_to_rgb (m_param.red.sgngamma (gamma).red, m_param.red.sgngamma (gamma).green, m_param.red.sgngamma (gamma).blue, &rr, &gg, &bb);
+  //printf ("red: %f %f %f\n",rr,gg,bb);
+  //m_color_adjust.apply_to_rgb (m_param.black.sgngamma (gamma).red, m_param.black.sgngamma (gamma).green, m_param.black.sgngamma (gamma).blue, &rr, &gg, &bb);
+  //printf ("black: %f %f %f\n",rr,gg,bb);
   //printf ("Inverse:\n");
   //m_color_adjust.print(stdout);
   //printf ("combined:\n");
