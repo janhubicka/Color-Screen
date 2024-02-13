a345bb50a576502265ab5f2ddda33567d55541ae
Jan Hubicka <hubicka@ucw.cz>
2024-02-13 21:19:25 +0100

    Unsigned int.

--- src/libcolorscreen/include/color.h
+++ src/libcolorscreen/include/color.h
@@ -30,17 +30,17 @@ struct mem_luminosity_t
   inline
   mem_luminosity_t (float y)
   {
-    const uint b = as_uint(y)+0x00001000; // round-to-nearest-even: add last bit after truncated mantissa
-    const uint e = (b&0x7F800000)>>23; // exponent
-    const uint m = b&0x007FFFFF; // mantissa; in line below: 0x007FF000 = 0x00800000-0x00001000 = decimal indicator flag - initial rounding
+    const unsigned int b = as_uint(y)+0x00001000; // round-to-nearest-even: add last bit after truncated mantissa
+    const unsigned int e = (b&0x7F800000)>>23; // exponent
+    const unsigned int m = b&0x007FFFFF; // mantissa; in line below: 0x007FF000 = 0x00800000-0x00001000 = decimal indicator flag - initial rounding
     x = (b&0x80000000)>>16 | (e>112)*((((e-112)<<10)&0x7C00)|m>>13) | ((e<113)&(e>101))*((((0x007FF000+m)>>(125-e))+1)>>1) | (e>143)*0x7FFF; // sign : normalized : denormalized : saturate
   }
   inline const_attr
   operator float () const
   {
-    const uint e = (x&0x7C00)>>10; // exponent
-    const uint m = (x&0x03FF)<<13; // mantissa
-    const uint v = as_uint((float)m)>>23; // evil log2 bit hack to count leading zeros in denormalized format
+    const unsigned int e = (x&0x7C00)>>10; // exponent
+    const unsigned int m = (x&0x03FF)<<13; // mantissa
+    const unsigned int v = as_uint((float)m)>>23; // evil log2 bit hack to count leading zeros in denormalized format
     return as_float((x&0x8000)<<16 | (e!=0)*((e+112)<<23|m) | ((e==0)&(m!=0))*((v-37)<<23|((m<<(150-v))&0x007FE000))); // sign : normalized : denormalized
   }
   inline const_attr
@@ -50,13 +50,13 @@ struct mem_luminosity_t
   }
 private:
   static inline const_attr
-  uint as_uint(const float x) {
+  unsigned int as_uint(const float x) {
     union u {float f; uint32_t i;} v;
     v.f = x;
     return v.i;
   }
   static inline const_attr
-  float as_float(const uint x) {
+  float as_float(const unsigned int x) {
     union u {float f; uint32_t i;} v;
     v.i = x;
     return v.f;
