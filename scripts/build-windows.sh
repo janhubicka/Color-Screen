159b62e184b2e7c44a722920584ae51fb979c91c
Jan Hubicka <hubicka@ucw.cz>
2024-05-05 01:34:19 +0200

    update script

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # first install MSYS2 packaging of mingw64 from https://www.mingw-w64.org/downloads/#msys2
 # then run this script
-pacman -S mingw-w64-x86_64-gcc mingw-w64-x86_64-gtk2 mingw-w64-x86_64-libtiff mingw-w64-x86_64-libjpeg-turbo git make diffutils automake autoconf mingw-w64-x86_64-pkg-config vim gdb libtool mingw-w64-x86_64-libraw mingw-w64-x86_64-lcms  mingw-w64-x86_64-libzip
+pacman -S mingw-w64-x86_64-gcc mingw-w64-x86_64-gtk2 mingw-w64-x86_64-libtiff mingw-w64-x86_64-libjpeg-turbo git make diffutils automake autoconf mingw-w64-x86_64-pkg-config vim gdb libtool mingw-w64-x86_64-libraw mingw-w64-x86_64-lcms  mingw-w64-x86_64-libzip mingw-w64-x86_64-gsl
 git clone https://github.com/janhubicka/Color-Screen.git ColorScreen
 mkdir ColorScreen-build
 cd ColorScreen-build/
-../ColorScreen/configure --prefix=/home/muzeu/barveni-bin --enable-gtkgui
+../ColorScreen/configure --prefix=~/barveni-bin --enable-gtkgui
 make
 make install
