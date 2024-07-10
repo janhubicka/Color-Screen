ee48078098c1b832f362aaf40bef065fbb79a477
Jan Hubicka <hubicka@ucw.cz>
2024-07-10 14:05:15 +0200

    add lru-cache-loc.h

---
+++ src/libcolorscreen/lru-cache-loc.h
@@ -0,0 +1,395 @@
+#ifndef LRU_CACHE
+#define LRU_CACHE
+#include <pthread.h>
+#include <atomic>
+#include <include/progress-info.h>
+class lru_caches
+{
+public:
+   constexpr lru_caches ()
+   {
+   }
+   static unsigned long get ()
+   {
+      return std::atomic_fetch_add(&time, 1);
+   }
+private:
+   static std::atomic_ulong time;
+};
+extern class lru_caches lru_caches;
+
+/* LRU cache used keep various data between invocations of renderers.
+   P represents parameters which are used to produce T.
+   get_new is a function computing T based on P.  It is expected to
+   allocate memory via new and lru_cache will eventually delete it.  */
+template<typename P, typename T, T *get_new (P &, progress_info *progress), int base_cache_size>
+class lru_cache
+{
+  static const bool debug = false;
+  static const bool verbose = false;
+public:
+  struct cache_entry
+  {
+    P params;
+    T *val;
+    cache_entry *next;
+    pthread_mutex_t lock;
+    unsigned long id;
+    unsigned long last_used;
+    std::atomic<int> nuses;
+  } *entries;
+
+  lru_cache(const char *n)
+  : entries (NULL), cache_size (base_cache_size), name (n)
+  {
+    if (pthread_mutex_init(&lock, NULL) != 0)
+      abort ();
+  }
+
+  void
+  prune ()
+  {
+    if (pthread_mutex_lock (&lock))
+      abort ();
+    struct cache_entry **e;
+    for (e = &entries; *e;)
+      {
+        if (!(*e)->nuses)
+	  {
+	    if (pthread_mutex_lock (&(*e)->lock))
+	      abort ();
+	    if ((*e)->nuses)
+	      {
+		pthread_mutex_unlock (&(*e)->lock);
+		continue;
+	      }
+	    if (verbose)
+	      fprintf (stderr, "Cache %s: deleting id %i\n", name, (int)(*e)->id);
+	    delete (*e)->val;
+	    struct cache_entry *next = (*e)->next;
+	    delete (*e);
+	    (*e) = next;
+	  }
+	else
+	 e = &(*e)->next;
+      }
+    pthread_mutex_unlock (&lock);
+  }
+
+  ~lru_cache()
+  {
+    prune ();
+    if (entries)
+      fprintf (stderr, "Claimed entries in cache %s. Leaking memory\n", name);
+    if (pthread_mutex_destroy(&lock) != 0)
+      abort ();
+  }
+  void
+  increase_capacity (int n)
+  {
+    if (pthread_mutex_lock (&lock))
+      abort ();
+    cache_size = n * base_cache_size;
+    pthread_mutex_unlock (&lock);
+  }
+
+  /* Get T for parameters P; do caching.
+     If ID is non-NULL initialize it to the unique identifier of the cached data.  */
+  T *get(P &p, progress_info *progress, unsigned long *id = NULL)
+  {
+    int size = 0;
+    unsigned long time = lru_caches::get ();
+    struct cache_entry *longest_unused = NULL, *e;
+    if (progress)
+      progress->set_task ("unlocking cache", 1);
+    if (pthread_mutex_lock (&lock))
+      abort ();
+    time++;
+    for (e = entries; e; e = e->next)
+      {
+	if (p == e->params)
+	  {
+	    if (pthread_mutex_init(&e->lock, NULL) != 0)
+	      abort ();
+	    if (p == e->params)
+	      {
+		e->last_used = time;
+		e->nuses++;
+		if (verbose)
+		  fprintf (stderr, "Cache %s: hit id %i nuses %i\n", name, (int)e->id, (int)e->nuses);
+		T *ret = e->val;
+		if (pthread_mutex_unlock (&lock))
+		  abort ();
+		if (pthread_mutex_unlock (&e->lock))
+		  abort ();
+		if (id)
+		  *id = e->id;
+		return ret;
+	      }
+	  }
+	if (!e->nuses
+	    && (!longest_unused || longest_unused->last_used < e->last_used))
+	  longest_unused = e;
+	size++;
+      }
+    if (size >= cache_size && longest_unused)
+      {
+        e = longest_unused;
+	if (verbose)
+	  fprintf (stderr, "Cache %s: deleting id %i\n", name, (int)e->id);
+	delete e->val;
+      }
+    else
+      {
+	if (debug && size >= cache_size)
+	  fprintf (stderr, "Cache %s is over capacity: %i %i\n", name, size + 1, cache_size);
+	e = new cache_entry;
+	if (!e)
+	  {
+	    pthread_mutex_unlock (&lock);
+	    return NULL;
+	  }
+        if (pthread_mutex_init(&e->lock, NULL) != 0)
+	  abort ();
+	e->next = entries;
+	entries = e;
+      }
+    if (pthread_mutex_lock (&e->lock))
+      abort ();
+    e->params = p;
+    e->nuses = 1;
+    e->id = time;
+    e->last_used = time;
+    pthread_mutex_unlock (&lock);
+    e->val = get_new (e->params, progress);
+    T *ret = e->val;
+    if (id)
+      *id = e->id;
+    if (!ret)
+      {
+	if (pthread_mutex_lock (&lock))
+	  abort ();
+	for (cache_entry **e2 = &entries; ; e2 = &(*e2)->next)
+	  if (*e2 == e)
+	    {
+	      *e2 = e->next;
+	      delete e;
+	      break;
+	    }
+	pthread_mutex_unlock (&lock);
+      }
+    pthread_mutex_unlock (&e->lock);
+    if (verbose)
+      fprintf (stderr, "Cache %s: added id %i size %i\n", name, (int)e->id, (int)size);
+    if (pthread_mutex_unlock (&e->lock))
+      abort ();
+    return ret;
+  }
+
+  /* Release T but keep it possibly in the cache.  */
+  void release(T *val)
+  {
+    if (pthread_mutex_lock (&lock))
+      abort ();
+    for (cache_entry *e = entries; ; e = e->next)
+      if (e->val == val)
+	{
+	  if (pthread_mutex_lock (&e->lock))
+	    abort ();
+	  pthread_mutex_unlock (&lock);
+	  e->nuses--;
+	  pthread_mutex_unlock (&e->lock);
+	  assert (e->nuses >= 0);
+	  if (verbose)
+	    fprintf (stderr, "Cache %s: reclaimed id %i nuses %i\n", name, (int)e->id, (int)e->nuses);
+	  return;
+	}
+    fprintf (stderr, "Released data not found in cache %s\n", name);
+  }
+private:
+  int cache_size;
+  pthread_mutex_t lock;
+  const char *name;
+};
+
+/* LRU cache used keep various data between invocations of renderers.
+   P represents parameters which are used to produce T.
+   get_new is a function computing T based on P.  It is expected to
+   allocate memory via new and lru_cache will eventually delete it.  */
+template<typename P, typename T, T *get_new (P &, int xshift, int yshift, int width, int height, progress_info *progress), int base_cache_size>
+class lru_tile_cache
+{
+  static const bool debug = false;
+  static const bool verbose = false;
+public:
+  struct cache_entry
+  {
+    P params;
+    T *val;
+    cache_entry *next;
+    int xshift, yshift, width, height;
+    unsigned long id;
+    unsigned long last_used;
+    std::atomic<int> nuses;
+  } *entries;
+
+  lru_tile_cache(const char *n)
+  : entries (NULL), cache_size (base_cache_size), name (n)
+  {
+    if (pthread_mutex_init(&lock, NULL) != 0)
+      abort ();
+  }
+
+  void
+  prune ()
+  {
+    if (pthread_mutex_lock (&lock))
+      abort ();
+    struct cache_entry **e;
+    for (e = &entries; *e;)
+      {
+        if (!(*e)->nuses)
+	  {
+	    if (verbose)
+	      fprintf (stderr, "Cache %s: deleting id %i\n", name, (int)(*e)->id);
+	    delete (*e)->val;
+	    struct cache_entry *next = (*e)->next;
+	    delete (*e);
+	    *e = next;
+	  }
+	else
+	 e = &(*e)->next;
+      }
+    pthread_mutex_unlock (&lock);
+  }
+
+  ~lru_tile_cache()
+  {
+    prune ();
+    if (entries)
+      fprintf (stderr, "Claimed entries in cache %s. Leaking memory\n", name);
+    if (pthread_mutex_destroy(&lock) != 0)
+      abort ();
+  }
+  void
+  increase_capacity (int n)
+  {
+    if (pthread_mutex_lock (&lock))
+      abort ();
+    cache_size = n * base_cache_size;
+    pthread_mutex_unlock (&lock);
+  }
+
+  /* Get T for parameters P; do caching.
+     If ID is non-NULL initialize it to the unique identifier of the cached data.  */
+  T *get(P &p, int xshift, int yshift, int width, int height, progress_info *progress, unsigned long *id = NULL)
+  {
+    int size = 0;
+    unsigned long time = lru_caches::get ();
+    struct cache_entry *longest_unused = NULL, *e;
+    if (pthread_mutex_lock (&lock))
+      abort ();
+    time++;
+    for (e = entries; e; e = e->next)
+      {
+	if (xshift <= e->xshift
+	    && yshift <= e->yshift
+	    && width - xshift <= e->width - e->xshift
+	    && height - yshift <= e->height - e->yshift
+	    && p == e->params)
+	  {
+	    e->last_used = time;
+	    e->nuses++;
+	    if (verbose)
+	      fprintf (stderr, "Cache %s: hit id %i nuses %i\n", name, (int)e->id, (int)e->nuses);
+	    T *ret = e->val;
+	    pthread_mutex_unlock (&lock);
+	    if (id)
+	      *id = e->id;
+	    return ret;
+	  }
+	if (!e->nuses
+	    && (!longest_unused || longest_unused->last_used < e->last_used))
+	  longest_unused = e;
+	size++;
+      }
+    if (size >= cache_size && longest_unused)
+      {
+        e = longest_unused;
+	if (verbose)
+	  fprintf (stderr, "Cache %s: deleting id %i\n", name, (int)e->id);
+	delete e->val;
+      }
+    else
+      {
+	if (debug && size >= cache_size)
+	  fprintf (stderr, "Cache %s is over capacity: %i %i\n", name, size + 1, cache_size);
+	e = new cache_entry;
+	if (!e)
+	  {
+	    pthread_mutex_unlock (&lock);
+	    return NULL;
+	  }
+	e->next = entries;
+	entries = e;
+      }
+    e->params = p;
+    e->xshift = xshift;
+    e->yshift = yshift;
+    e->width = width;
+    e->height = height;
+    e->val = get_new (e->params, xshift, yshift, width, height, progress);
+    e->nuses = 1;
+    e->id = time;
+    e->last_used = time;
+    T *ret = e->val;
+    if (id)
+      *id = e->id;
+    if (!ret)
+      {
+	for (cache_entry **e2 = &entries; ; e2 = &(*e2)->next)
+	  if (*e2 == e)
+	    {
+	      *e2 = e->next;
+	      delete e;
+	      break;
+	    }
+      }
+    if (verbose)
+      fprintf (stderr, "Cache %s: added id %i size %i\n", name, (int)e->id, (int)size);
+    pthread_mutex_unlock (&lock);
+    return ret;
+  }
+
+  /* Release T but keep it possibly in the cache.  */
+  void release(T *val)
+  {
+    pthread_mutex_lock (&lock);
+    for (cache_entry *e = entries; ; e = e->next)
+      if (e->val == val)
+	{
+	  e->nuses--;
+	  assert (e->nuses >= 0);
+	  if (verbose)
+	    fprintf (stderr, "Cache %s: reclaimed id %i nuses %i\n", name, (int)e->id, (int)e->nuses);
+	  pthread_mutex_unlock (&lock);
+	  return;
+	}
+    fprintf (stderr, "Released data not found in cache %s\n", name);
+  }
+private:
+  int cache_size;
+  pthread_mutex_t lock;
+  const char *name;
+};
+
+extern DLL_PUBLIC void render_increase_lru_cache_sizes_for_stitch_projects (int n);
+extern DLL_PUBLIC void render_interpolated_increase_lru_cache_sizes_for_stitch_projects (int n);
+inline void
+increase_lru_cache_sizes_for_stitch_projects (int n)
+{
+   render_increase_lru_cache_sizes_for_stitch_projects (n);
+   render_interpolated_increase_lru_cache_sizes_for_stitch_projects (n);
+}
+
+#endif
