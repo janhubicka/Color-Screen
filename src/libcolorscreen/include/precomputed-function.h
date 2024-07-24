99d31608ed722711b91b0a78bac3aadb4f7f97ef
Jan Hubicka <hubicka@ucw.cz>
2024-07-24 14:46:12 +0200

    scr-to-img-parameters cleanups

--- src/libcolorscreen/include/precomputed-function.h
+++ src/libcolorscreen/include/precomputed-function.h
@@ -18,12 +18,12 @@ my_floor (float x)
 
 /* Lookup table defined 1d function.  */
 
-template<typename T> class precomputed_function
+template <typename T> class precomputed_function
 {
-  public:
+public:
   constexpr
   precomputed_function ()
-  : m_min_x (0), m_max_x (0), m_entries (0)
+      : m_min_x (0), m_max_x (0), m_entries (0)
   {
   }
   void
@@ -34,18 +34,20 @@ template<typename T> class precomputed_function
   }
   constexpr
   precomputed_function (T min_x, T max_x)
-  : m_min_x (min_x), m_max_x (max_x), m_entries (0)
+      : m_min_x (min_x), m_max_x (max_x), m_entries (0)
   {
   }
-  /* Constructor based on a known table of LEN values rangling from MIN_X to MAX_X.  */
+  /* Constructor based on a known table of LEN values rangling from MIN_X to
+     MAX_X.  */
   precomputed_function (T min_x, T max_x, const T *y, int len)
-  : m_min_x (min_x), m_max_x (max_x)
-    {
-      init_by_y_values (y, len);
-    }
+      : m_min_x (min_x), m_max_x (max_x)
+  {
+    init_by_y_values (y, len);
+  }
   /* Construct linear interpolation between known X and Y values.  */
-  precomputed_function (T min_x, T max_x, int len, const T *x, const T *y, int npoints)
-  : m_min_x (min_x), m_max_x (max_x)
+  precomputed_function (T min_x, T max_x, int len, const T *x, const T *y,
+                        int npoints)
+      : m_min_x (min_x), m_max_x (max_x)
   {
     /* Sanitize input. */
     if (m_min_x < x[0])
@@ -57,8 +59,8 @@ template<typename T> class precomputed_function
     /* If there are only 2 npoints we represent linear function.  */
     if (npoints <= 2)
       {
-	m_entries = 1;
-	len = 2;
+        m_entries = 1;
+        len = 2;
       }
     T *yy = (T *)malloc (sizeof (entry) * len);
     T step = (m_max_x - m_min_x) / (T)(len - 1);
@@ -66,31 +68,32 @@ template<typename T> class precomputed_function
     /* If there is no control point just define function as f(x)=x.  */
     if (!npoints)
       {
-	for (int i = 0; i < len; i++)
-	  yy[i] = m_min_x + i * step;
+        for (int i = 0; i < len; i++)
+          yy[i] = m_min_x + i * step;
       }
     /* If there is one control point, use f(x) = x + c.  */
     else if (npoints == 1)
       {
-	for (int i = 0; i < len; i++)
-	  yy[i] = m_min_x + i * step + y[0] - x[0];
+        for (int i = 0; i < len; i++)
+          yy[i] = m_min_x + i * step + y[0] - x[0];
       }
     else
       for (int i = 0, p = 0; i < len; i++)
-	{
-	  T xx = m_min_x + i * step;
-	  while (p < npoints - 1 && x[p+1] < xx)
-	    p++;
-	  yy[i] = y[p] + (y[p+1]-y[p]) * (xx - x[p]) / (x[p+1]-x[p]);
-	}
+        {
+          T xx = m_min_x + i * step;
+          while (p < npoints - 1 && x[p + 1] < xx)
+            p++;
+          yy[i] = y[p] + (y[p + 1] - y[p]) * (xx - x[p]) / (x[p + 1] - x[p]);
+        }
     init_by_y_values (yy, len);
     free (yy);
   }
 
-  /* Construct linear interpolation between known X and Y values but organized in
-     single array of pairs instead of two separated arrays.  */
-  precomputed_function (T min_x, T max_x, int len, const T table[][2], int npoints)
-  : m_min_x (min_x), m_max_x (max_x)
+  /* Construct linear interpolation between known X and Y values but organized
+     in single array of pairs instead of two separated arrays.  */
+  precomputed_function (T min_x, T max_x, int len, const T table[][2],
+                        int npoints)
+      : m_min_x (min_x), m_max_x (max_x)
   {
     /* Sanitize input. */
     if (m_min_x < table[0][0])
@@ -102,81 +105,88 @@ template<typename T> class precomputed_function
     /* If there are only 2 npoints we represent linear function.  */
     if (npoints <= 2)
       {
-	m_entries = 1;
-	len = 2;
+        m_entries = 1;
+        len = 2;
       }
     T *yy = (T *)malloc (sizeof (entry) * len);
     T step = (m_max_x - m_min_x) / (T)(len - 1);
     /* If there is no control point just define function as f(x)=x.  */
     if (!npoints)
       {
-	for (int i = 0; i < len; i++)
-	  yy[i] = m_min_x + i * step;
+        for (int i = 0; i < len; i++)
+          yy[i] = m_min_x + i * step;
       }
     /* If there is one control point, use f(x) = x + c.  */
     else if (npoints == 1)
       {
-	for (int i = 0; i < len; i++)
-	  yy[i] = m_min_x + i * step + table[0][0] - table[0][0];
+        for (int i = 0; i < len; i++)
+          yy[i] = m_min_x + i * step + table[0][0] - table[0][0];
       }
     else
       for (int i = 0, p = 0; i < len; i++)
-	{
-	  T xx = m_min_x + i * step;
-	  while (p < npoints - 1 && table[p+1][0] < xx)
-	    p++;
-	  yy[i] = table[p][1] + (table[p+1][1]-table[p][1]) * (xx - table[p][0]) / (table[p+1][0]-table[p][0]);
-	}
+        {
+          T xx = m_min_x + i * step;
+          while (p < npoints - 1 && table[p + 1][0] < xx)
+            p++;
+          yy[i] = table[p][1]
+                  + (table[p + 1][1] - table[p][1]) * (xx - table[p][0])
+                        / (table[p + 1][0] - table[p][0]);
+        }
     init_by_y_values (yy, len);
     free (yy);
   }
 
   ~precomputed_function ()
-    {
-      if (m_entries)
-	free (m_entry);
-    }
+  {
+    if (m_entries)
+      free (m_entry);
+  }
 
   /* Return f(x).  */
-  const T pure_attr 
-  apply (T x)
-    {
-      int index = my_floor ((x - m_min_x) * m_step_inv);
-      index = std::max (std::min (index, m_entries - 1), 0);
-      //printf ("x:%f index:%i add %f slope %f\n", x, index, m_entry[index].add, m_entry[index].slope);
-      return m_entry[index].add + m_entry[index].slope * x;
-    }
+  T pure_attr
+  apply (T x) const
+  {
+    int index = my_floor ((x - m_min_x) * m_step_inv);
+    index = std::max (std::min (index, m_entries - 1), 0);
+    return m_entry[index].add + m_entry[index].slope * x;
+  }
 
   /* Determine inverse.  Works only for monotone functions.  */
   T pure_attr
-  invert (T y)
+  invert (T y) const
   {
     unsigned int min = 0;
     unsigned int max = m_entries;
     while (max != min)
       {
-	unsigned int ix = (min + max) / 2;
-	T xx = m_min_x + ix * m_step;
-	T val = m_entry[ix].add + m_entry[ix].slope * xx;
-	T val2 = val + m_entry[ix].slope * m_step;
-	if (!increasing)
-	  std::swap (val, val2);
-	if (val <= y && y <= val2)
-	  min = max = ix;
-	else if ((val < y) ^ increasing)
-	  max = ix;
-	else if (min != ix)
-	  min = ix;
-	else
-	  break;
+        unsigned int ix = (min + max) / 2;
+        T xx = m_min_x + ix * m_step;
+        T val = m_entry[ix].add + m_entry[ix].slope * xx;
+        T val2 = val + m_entry[ix].slope * m_step;
+        if (!increasing)
+          std::swap (val, val2);
+        if (val <= y && y <= val2)
+          min = max = ix;
+        else if ((val < y) ^ increasing)
+          max = ix;
+        else if (min != ix)
+          min = ix;
+        else
+          break;
       }
-    double ret = m_entry[min].slope ? (y - m_entry[min].add) / m_entry[min].slope : m_min_x + min * m_step;
+    double ret = m_entry[min].slope
+                     ? (y - m_entry[min].add) / m_entry[min].slope
+                     : m_min_x + min * m_step;
 
-    /* Verify that inverse works.  This also crashes if the function is not monotone.  */
+    /* Verify that inverse works.  This also crashes if the function is not
+       monotone.  */
     if (debug && fabs (apply (ret) - y) > epsilon)
       {
-	printf ("%i %i:%f...%i:%f min:%i (%f..%f) ret:%f %f should be %f\n", increasing, 0,m_min_x, m_entries - 1, m_max_x, min, m_min_x + min * m_step, m_min_x + (min+1) * m_step, ret, apply (ret), y);
-	abort ();
+        printf ("%i %i:%f...%i:%f min:%i (%f..%f) ret:%f %f should be %f\n",
+                increasing, 0, m_min_x, m_entries - 1, m_max_x, min,
+                m_min_x + min * m_step, m_min_x + (min + 1) * m_step, ret,
+                apply (ret), y);
+        abort ();
       }
     return ret;
   }
@@ -194,9 +204,9 @@ template<typename T> class precomputed_function
     m_step_inv = 1 / m_step;
     for (int i = 0; i < len - 1; i++)
       {
-	T xleft = m_min_x + i * m_step;
-	m_entry[i].slope = (y[i+1] - y[i]) * m_step_inv;
-	m_entry[i].add = y[i] - xleft * m_entry[i].slope;
+        T xleft = m_min_x + i * m_step;
+        m_entry[i].slope = (y[i + 1] - y[i]) * m_step_inv;
+        m_entry[i].add = y[i] - xleft * m_entry[i].slope;
       }
     increasing = y[0] < y[len - 1];
   }
@@ -205,12 +215,11 @@ private:
   const T epsilon = 0.001;
   T m_min_x, m_max_x, m_step, m_step_inv;
   int m_entries;
-  struct entry {
+  struct entry
+  {
     T slope, add;
   } *m_entry;
   bool increasing;
   static const bool debug = colorscreen_checking;
 };
-
-
 #endif
--- src/libcolorscreen/include/scr-to-img.h
+++ src/libcolorscreen/include/scr-to-img.h
@@ -9,6 +9,7 @@
 #include "precomputed-function.h"
 #include "mesh.h"
 #include "lens-correction.h"
+#include "scr-to-img-parameters.h"
 
 /* Windows does not seem to define this by default.  */
 #ifndef M_PI
@@ -19,240 +20,6 @@
 typedef matrix4x4<coord_t> trans_4d_matrix;
 typedef matrix3x3<coord_t> trans_3d_matrix;
 typedef matrix2x2<coord_t> trans_2d_matrix;
-class render;
-struct render_parameters;
-
-/* Types of supported screens.  */
-enum scr_type
-{
-   Random,
-   Paget,
-   Thames,
-   Finlay,
-   Dufay,
-   max_scr_type
-};
-DLL_PUBLIC extern const char * const scr_names[max_scr_type];
-DLL_PUBLIC rgbdata patch_proportions (enum scr_type t, render_parameters *);
-
-/* Type of a scanner used.  */
-enum scanner_type {
-	fixed_lens,
-	fixed_lens_sensor_move_horisontally,
-	fixed_lens_sensor_move_vertically,
-	lens_move_horisontally,
-	lens_move_vertically,
-	max_scanner_type
-};
-
-inline bool is_fixed_lens (scanner_type type)
-{
-  return type == fixed_lens || type == fixed_lens_sensor_move_horisontally || type == fixed_lens_sensor_move_vertically;
-}
-
-DLL_PUBLIC extern const char * const scanner_type_names[max_scanner_type];
-
-/* This implements to translate image coordiantes to coordinates of the viewing screen.
-   In the viewing screen the coordinats (0,0) describe a green dot and
-   the screen is periodic with period 1: that is all integer coordinates describes
-   gren dots again.
- 
-   In order to turn scan coordinates to screen the following transformations
-   are performed
-     1) motor correction
-     2) translation to move lens_center to (0,0)
-     3) lens correction
-     4) perspective correction (with tilt applied)
-     5) translation to move center to (0,0)
-     6) change of basis so center+coordinate1 becomes (1.0) and center+coordinate2 becomes (0,1)
-*/
-
-struct scr_to_img_parameters
-{
-  /* Coordinates (in the image) of the center of the screen (a green dot).  */
-  coord_t center_x, center_y;
-  /* First coordinate vector:
-     image's (center_x+coordinate1_x, centr_y+coordinate1_y) should describe
-     a green dot just on the right side of (center_x, center_y).  */
-  coord_t coordinate1_x, coordinate1_y;
-  /* Second coordinate vector:
-     image's (center_x+coordinate1_x, centr_y+coordinate1_y) should describe
-     a green dot just below (center_x, center_y).  */
-  coord_t coordinate2_x, coordinate2_y;
-
-  /* Distance of the perspective pane from the camera coordinate.  */
-  coord_t projection_distance;
-  /* Perspective tilt in x and y coordinate in degrees.  */
-  coord_t tilt_x, tilt_y;
-
-  /* Rotation from screen coordinates to final coordinates.  */
-  coord_t final_rotation;
-  /* Angle of the screen X and Y axis in the final coordinates.  */
-  coord_t final_angle;
-  /* Ratio of the X and Y axis in the final coordinates.  */
-  coord_t final_ratio;
-
-  /* Stepping motor correction is described by a spline.  */
-  coord_t *motor_correction_x, *motor_correction_y;
-  int n_motor_corrections;
-
-  mesh *mesh_trans;
-
-  enum scr_type type;
-  enum scanner_type scanner_type;
-
-  lens_warp_correction_parameters lens_correction;
-
-  scr_to_img_parameters ()
-  : center_x (0), center_y (0), coordinate1_x(5), coordinate1_y (0), coordinate2_x (0), coordinate2_y (5),
-    projection_distance (1), tilt_x (0), tilt_y(0), 
-    final_rotation (0), final_angle (90), final_ratio (1), motor_correction_x (NULL), motor_correction_y (NULL),
-    n_motor_corrections (0), mesh_trans (NULL), type (Finlay), scanner_type (fixed_lens), lens_correction ()
-  { }
-  scr_to_img_parameters (const scr_to_img_parameters &from)
-  : center_x (from.center_x), center_y (from.center_y),
-    coordinate1_x(from.coordinate1_x), coordinate1_y (from.coordinate1_y),
-    coordinate2_x (from.coordinate2_x), coordinate2_y (from.coordinate2_y),
-    projection_distance (from.projection_distance), tilt_x (from.tilt_x), tilt_y(from.tilt_y), 
-    final_rotation (from.final_rotation), final_angle (from.final_angle), final_ratio (from.final_ratio), motor_correction_x (NULL), motor_correction_y (NULL),
-    n_motor_corrections (from.n_motor_corrections),
-    mesh_trans (from.mesh_trans), type (from.type), scanner_type (from.scanner_type), lens_correction (from.lens_correction)
-  {
-    if (n_motor_corrections)
-      {
-        motor_correction_x = (coord_t *)malloc (n_motor_corrections * sizeof (coord_t));
-        motor_correction_y = (coord_t *)malloc (n_motor_corrections * sizeof (coord_t));
-	memcpy (motor_correction_x, from.motor_correction_x, n_motor_corrections * sizeof (coord_t));
-	memcpy (motor_correction_y, from.motor_correction_y, n_motor_corrections * sizeof (coord_t));
-      }
-  }
-  scr_to_img_parameters &operator= (const scr_to_img_parameters &other)
-  {
-    free (motor_correction_x);
-    free (motor_correction_y);
-    n_motor_corrections = 0;
-    copy_from_cheap (other);
-    n_motor_corrections = other.n_motor_corrections;
-    motor_correction_x = (coord_t *)malloc (n_motor_corrections * sizeof (coord_t));
-    motor_correction_y = (coord_t *)malloc (n_motor_corrections * sizeof (coord_t));
-    memcpy (motor_correction_x, other.motor_correction_x, n_motor_corrections * sizeof (coord_t));
-    memcpy (motor_correction_y, other.motor_correction_y, n_motor_corrections * sizeof (coord_t));
-    return *this;
-  }
-  /* Copy everything except for motor corrections.  */
-  void
-  copy_from_cheap (const scr_to_img_parameters &from)
-  {
-    center_x = from.center_x;
-    center_y = from.center_y;
-    coordinate1_x = from.coordinate1_x;
-    coordinate1_y = from.coordinate1_y;
-    coordinate2_x = from.coordinate2_x;
-    coordinate2_y = from.coordinate2_y;
-    projection_distance = from.projection_distance;
-    tilt_x = from.tilt_x;
-    tilt_y = from.tilt_y;
-    final_rotation = from.final_rotation;
-    final_angle = from.final_angle;
-    final_ratio = from.final_ratio;
-    type = from.type;
-    scanner_type = from.scanner_type;
-    mesh_trans = from.mesh_trans;
-    lens_correction = from.lens_correction;
-    if (n_motor_corrections)
-      abort ();
-  }
-  ~scr_to_img_parameters ()
-  {
-    free (motor_correction_x);
-    free (motor_correction_y);
-  }
-  bool operator== (scr_to_img_parameters &other) const
-  {
-    if (n_motor_corrections != other.n_motor_corrections)
-      return false;
-    for (int i = 0; i < n_motor_corrections; i++)
-      if (motor_correction_x[i] != other.motor_correction_x[i]
-	  || motor_correction_y[i] != other.motor_correction_y[i])
-	return false;
-    return center_x == other.center_x
-	   && center_y == other.center_y
-	   && coordinate1_x == other.coordinate1_x
-	   && coordinate1_y == other.coordinate1_y
-	   && coordinate2_x == other.coordinate2_x
-	   && coordinate2_y == other.coordinate2_y
-	   && projection_distance == other.projection_distance
-	   && final_rotation == other.final_rotation
-	   && final_angle == other.final_angle
-	   && final_ratio == other.final_ratio
-	   && tilt_x == other.tilt_x
-	   && tilt_y == other.tilt_y
-	   && type == other.type
-	   && scanner_type == other.scanner_type
-	   && lens_correction == other.lens_correction;
-  }
-  bool operator!= (scr_to_img_parameters &other) const
-  {
-    return !(*this == other);
-  }
-  int
-  add_motor_correction_point (coord_t x, coord_t y)
-  {
-    int p = 0;
-
-    motor_correction_x = (coord_t *)realloc ((void *)motor_correction_x, (n_motor_corrections + 1) * sizeof (coord_t));
-    motor_correction_y = (coord_t *)realloc ((void *)motor_correction_y, (n_motor_corrections + 1) * sizeof (coord_t));
-    for (p = n_motor_corrections; p > 0 && motor_correction_x[p-1] > x; p--)
-      ;
-    for (int p2 = n_motor_corrections; p2 > p; p2 --)
-      {
-	motor_correction_x[p2] = motor_correction_x[p2 - 1];
-	motor_correction_y[p2] = motor_correction_y[p2 - 1];
-      }
-    motor_correction_x[p] = x;
-    motor_correction_y[p] = y;
-    n_motor_corrections++;
-    return p;
-  }
-  void
-  remove_motor_correction_point (int i)
-  {
-    for (; i < n_motor_corrections; i++)
-    {
-	motor_correction_x[i] = motor_correction_x[i + 1];
-	motor_correction_y[i] = motor_correction_y[i + 1];
-    }
-    n_motor_corrections--;
-  }
-  coord_t
-  screen_per_inch ()
-  {
-    if (type == Dufay)
-      /* Dufaycolor manual promises 500 lines per inch.  Screen has 2 lines  */
-      return 250;
-    else
-      /* 2 squares per screen.  */
-      return 25.4 / 2;
-  }
-  coord_t
-  get_xlen ()
-  {
-    return sqrt (coordinate1_x * coordinate1_x + coordinate1_y * coordinate1_y);
-  }
-  coord_t
-  get_ylen ()
-  {
-    return sqrt (coordinate2_x * coordinate2_x + coordinate2_y * coordinate2_y);
-  }
-  coord_t
-  get_angle ()
-  {
-    coord_t dot = coordinate1_x*coordinate2_x + coordinate1_y*coordinate2_y;
-    //coord_t det = coordinate1_x*coordinate2_y - coordinate1_y*coordinate2_x;
-    //return atan2(det, dot) * 180 / M_PI;
-    return acos (dot / (get_xlen () * get_ylen ())) * (180 / M_PI);
-  }
-};
 
 class image_data;
 
@@ -442,7 +209,8 @@ public:
 	  {
 	    printf ("Warning: to_scr is not inverted by to_img %f %f turns to %f %f\n", x, y, xx, yy);
 	    m_nwarnings++;
-	    //abort ();
+	    if (colorscreen_checking)
+	      abort ();
 	  }
       }
   }
@@ -519,7 +287,7 @@ private:
 
   /* Apply spline defining motor correction.  */
   void
-  apply_motor_correction (coord_t x, coord_t y, coord_t *xr, coord_t *yr)
+  apply_motor_correction (coord_t x, coord_t y, coord_t *xr, coord_t *yr) const
   {
     *xr = x;
     *yr = y;
@@ -531,7 +299,7 @@ private:
       *yr = m_motor_correction->apply (y);
   }
   void
-  inverse_motor_correction (coord_t x, coord_t y, coord_t *xr, coord_t *yr)
+  inverse_motor_correction (coord_t x, coord_t y, coord_t *xr, coord_t *yr) const
   {
     *xr = x;
     *yr = y;
