435f040eb3df323b8a5f6d7c6e79cc1061744d1d
Jan Hubicka <hubicka@ucw.cz>
2024-08-03 13:13:28 +0200

    Fix signedness warnings in bitmap.h; indent

--- src/libcolorscreen/bitmap.h
+++ src/libcolorscreen/bitmap.h
@@ -6,17 +6,14 @@ namespace colorscreen
 class bitmap
 {
 public:
-  bitmap(size_t size1)
+  bitmap (size_t size1)
   {
     if (colorscreen_checking)
       assert (size1 >= 0);
     size = size1;
     data = (uint8_t *)calloc ((size + 7) / 8, 1);
   }
-  ~bitmap()
-  {
-    free (data);
-  }
+  ~bitmap () { free (data); }
   bool
   test_bit (size_t p)
   {
@@ -53,55 +50,56 @@ public:
   {
     memset (data, 0, (size + 7) / 8);
   }
+
 protected:
   uint8_t *data;
-  int size;
+  size_t size;
 };
 
 /* Constanly sized 2d bitmap datastructure.  */
-class bitmap_2d: private bitmap
+class bitmap_2d : private bitmap
 {
 public:
-  bitmap_2d(int width1, int height1)
-  : bitmap (width1 * height1), width (width1), height (height1)
+  bitmap_2d (int width1, int height1)
+      : bitmap (width1 * height1), width (width1), height (height1)
   {
   }
-  ~bitmap_2d()
-  {
-  }
+  ~bitmap_2d () {}
   bool
-  test_bit (size_t x, size_t y)
+  test_bit (int64_t x, int64_t y)
   {
     if (colorscreen_checking)
-      assert (x >= 0 && y >= 0 && x <= width && y <= height);
+      assert (x >= 0 && y >= 0 && (size_t)x <= width && (size_t)y <= height);
     return bitmap::test_bit (y * width + x);
   }
   bool
   test_bit (int_point_t p)
   {
     if (colorscreen_checking)
-      assert (p.x >= 0 && p.y >= 0 && p.x <= width && p.y <= height);
+      assert (p.x >= 0 && p.y >= 0 && (size_t)p.x <= width
+              && (size_t)p.y <= height);
     return bitmap::test_bit (p.y * width + p.x);
   }
   bool
-  set_bit (size_t x, size_t y)
+  set_bit (int64_t x, int64_t y)
   {
     if (colorscreen_checking)
-      assert (x >= 0 && y >= 0 && x <= width && y <= height);
+      assert (x >= 0 && y >= 0 && (size_t)x <= width && (size_t)y <= height);
     return bitmap::set_bit (y * width + x);
   }
   bool
   set_bit (int_point_t p)
   {
     if (colorscreen_checking)
-      assert (p.x >= 0 && p.y >= 0 && p.x <= width && p.y <= height);
+      assert (p.x >= 0 && p.y >= 0 && (size_t)p.x <= width
+              && (size_t)p.y <= height);
     return bitmap::set_bit (p.y * width + p.x);
   }
   bool
-  clear_bit (size_t x, size_t y)
+  clear_bit (int64_t x, int64_t y)
   {
     if (colorscreen_checking)
-      assert (x >= 0 && y >= 0 && x <= width && y <= height);
+      assert (x >= 0 && y >= 0 && (size_t)x <= width && (size_t)y <= height);
     return bitmap::clear_bit (y * width + x);
   }
   bool
@@ -112,14 +110,15 @@ public:
     return bitmap::clear_bit (p.y * width + p.x);
   }
   bool
-  test_range (size_t x, size_t y, size_t r)
+  test_range (int_point_t p, int r)
   {
-    if (x < r || y < r || x + r >= (size_t)width || y + r >= (size_t)height)
+    if (p.x < (int)r || p.y < (int)r || (size_t)p.x + r >= (size_t)width
+        || (size_t)p.y + r >= (size_t)height)
       return false;
-    for (size_t yy = y - r; yy < y + r; yy++)
-      for (size_t xx = x - r; xx < x + r; xx++)
-	if (!test_bit (xx, yy))
-	  return false;
+    for (size_t yy = p.y - r; yy < p.y + r; yy++)
+      for (size_t xx = p.x - r; xx < p.x + r; xx++)
+        if (!test_bit (xx, yy))
+          return false;
     return true;
   }
   void
@@ -127,7 +126,7 @@ public:
   {
     bitmap::clear ();
   }
-  int width, height;
+  size_t width, height;
 };
 }
 #endif
