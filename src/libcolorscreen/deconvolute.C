2d09c063abc2415a4c064b0710db47fea2a1cbf8
Jan Hubicka <hubicka@ucw.cz>
2025-12-21 19:54:21 +0100

    Deconvolution

--- src/libcolorscreen/deconvolute.C
+++ src/libcolorscreen/deconvolute.C
@@ -6,10 +6,16 @@ namespace colorscreen
 /* FFTW execute is thread safe. Everything else is not.  */
 std::mutex fftw_lock;
 
-deconvolution::deconvolution (precomputed_function<luminosity_t> *mtf)
-    : m_border_size (256), m_tile_size (4 * 256),
-      m_blur_kernel (NULL)
+deconvolution::deconvolution (precomputed_function<luminosity_t> *mtf,
+                              int max_threads, bool sharpen)
+    : m_border_size (256), m_tile_size (4 * 256), m_blur_kernel (NULL)
 {
+  double snr = 1000;
+  double k_const = 1.0f / snr;
+
+  m_plans.resize (max_threads);
+  for (int i = 0; i < max_threads; i++)
+    m_plans[i].initialized = false;
   /* Result of real fft is symmetric.  We need only N /2 + 1 complex values.
      Moreover point spread functions we compute are symmetric real functions so
      the FFT result is again a real function (all complex values should be 0 up
@@ -17,36 +23,55 @@ deconvolution::deconvolution (precomputed_function<luminosity_t> *mtf)
   m_fft_size = m_tile_size / 2 + 1;
   m_blur_kernel = new fftw_complex[m_tile_size * m_fft_size];
   double scale = 1.0 / (m_tile_size * m_tile_size);
+  double rev_tile_size = 1 / m_tile_size;
   for (int y = 0; y < m_fft_size; y++)
     for (int x = 0; x < m_fft_size; x++)
       {
-        //deconvolution_data_t w = x > 0 || y > 0 ? mtf->apply (1 / sqrt (x * x + y * y)) * scale : scale;
-        deconvolution_data_t w = mtf->apply (sqrt (x * x + y * y) / m_tile_size) * scale;
-	//if (w < 0.01 * scale)
-	  //w = 0.01 * scale;
-        m_blur_kernel[y * m_fft_size + x][0] = w;
-        m_blur_kernel[y * m_fft_size + x][1] = 0;
+        std::complex ker ((deconvolution_data_t)mtf->apply (
+                              sqrt (x * x + y * y) * rev_tile_size),
+                          (deconvolution_data_t)0);
+
+        // If sharpening, apply Wiener Filter
+        // Result = Img * conj(Ker) / (|Ker|^2 + 1/SNR)
+        if (sharpen)
+          ker = conj (ker) / (std::norm (ker) + k_const);
+        ker = ker * scale;
+        m_blur_kernel[y * m_fft_size + x][0] = real (ker);
+        m_blur_kernel[y * m_fft_size + x][1] = imag (ker);
         if (y)
           {
-            m_blur_kernel[(m_tile_size - y) * m_fft_size + x][0] = w;
-            m_blur_kernel[(m_tile_size - y) * m_fft_size + x][1] = 0;
+            m_blur_kernel[(m_tile_size - y) * m_fft_size + x][0] = real (ker);
+            m_blur_kernel[(m_tile_size - y) * m_fft_size + x][1] = imag (ker);
           }
       }
 }
 
+/* Allocate memory for tiles and initialize fftw plans for given thread.  */
 void
-deconvolution::blur_tile (deconvolution_data_t *d)
+deconvolution::init (int thread_id)
+{
+  if (m_plans[thread_id].initialized)
+    return;
+  fftw_lock.lock ();
+  m_plans[thread_id].in = new fftw_complex[m_tile_size * m_fft_size];
+  m_plans[thread_id].tile.resize (m_tile_size * m_tile_size);
+  m_plans[thread_id].plan_2d_inv
+      = fftw_plan_dft_c2r_2d (m_tile_size, m_tile_size, m_plans[thread_id].in,
+                              m_plans[thread_id].tile.data (), FFTW_ESTIMATE);
+  m_plans[thread_id].plan_2d = fftw_plan_dft_r2c_2d (
+      m_tile_size, m_tile_size, m_plans[thread_id].tile.data (),
+      m_plans[thread_id].in, FFTW_ESTIMATE);
+  m_plans[thread_id].initialized = true;
+  fftw_lock.unlock ();
+}
+
+/* Apply the kernel.  */
+void
+deconvolution::process_tile (int thread_id)
 {
   // std::vector<deconvolution_data_t> out(m_tile_size * m_tile_size);
-  fftw_plan plan_2d_inv, plan_2d;
-  std::vector<fftw_complex> in (m_tile_size * m_fft_size);
-  fftw_lock.lock ();
-  plan_2d_inv = fftw_plan_dft_c2r_2d (m_tile_size, m_tile_size, in.data (), d,
-                                      FFTW_ESTIMATE);
-  plan_2d = fftw_plan_dft_r2c_2d (m_tile_size, m_tile_size, d, in.data (),
-                                  FFTW_ESTIMATE);
-  fftw_lock.unlock ();
-  fftw_execute (plan_2d);
+  fftw_execute (m_plans[thread_id].plan_2d);
+  fftw_complex *in = m_plans[thread_id].in;
   for (int i = 0; i < m_fft_size * m_tile_size; i++)
     {
       std::complex w (m_blur_kernel[i][0], m_blur_kernel[i][1]);
@@ -54,37 +79,20 @@ deconvolution::blur_tile (deconvolution_data_t *d)
       in[i][0] = real (v * w);
       in[i][1] = imag (v * w);
     }
-  fftw_execute (plan_2d_inv);
-  fftw_lock.lock ();
-  fftw_destroy_plan (plan_2d);
-  fftw_destroy_plan (plan_2d_inv);
-  fftw_lock.unlock ();
+  fftw_execute (m_plans[thread_id].plan_2d_inv);
 }
-void
-deconvolution::sharpen_tile (deconvolution_data_t *d)
+
+deconvolution::~deconvolution ()
 {
-  // std::vector<deconvolution_data_t> out(m_tile_size * m_tile_size);
-  fftw_plan plan_2d_inv, plan_2d;
-  std::vector<fftw_complex> in (m_tile_size * m_fft_size);
+  delete m_blur_kernel;
   fftw_lock.lock ();
-  plan_2d_inv = fftw_plan_dft_c2r_2d (m_tile_size, m_tile_size, in.data (), d,
-                                      FFTW_ESTIMATE);
-  plan_2d = fftw_plan_dft_r2c_2d (m_tile_size, m_tile_size, d, in.data (),
-                                  FFTW_ESTIMATE);
-  fftw_lock.unlock ();
-  fftw_execute (plan_2d);
-  double scale = 1.0 / (m_tile_size * m_tile_size);
-  for (int i = 0; i < m_fft_size * m_tile_size; i++)
-    {
-      std::complex w (m_blur_kernel[i][0], m_blur_kernel[i][1]);
-      std::complex v (in[i][0], in[i][1]);
-      in[i][0] = real (v / w * scale * scale);
-      in[i][1] = imag (v / w * scale * scale);
-    }
-  fftw_execute (plan_2d_inv);
-  fftw_lock.lock ();
-  fftw_destroy_plan (plan_2d);
-  fftw_destroy_plan (plan_2d_inv);
+  for (size_t i = 0; i < m_plans.size (); i++)
+    if (m_plans[i].initialized)
+      {
+        fftw_destroy_plan (m_plans[i].plan_2d);
+        fftw_destroy_plan (m_plans[i].plan_2d_inv);
+        delete (m_plans[i].in);
+      }
   fftw_lock.unlock ();
 }
 
--- src/libcolorscreen/deconvolute.h
+++ src/libcolorscreen/deconvolute.h
@@ -1,5 +1,6 @@
 #ifndef DECONVOLUTE_H
 #define DECONVOLUTE_H
+#include <omp.h>
 #include "include/base.h"
 #include "include/color.h"
 #include "include/precomputed-function.h"
@@ -13,12 +14,11 @@ extern std::mutex fftw_lock;
 class deconvolution
 {
 public:
-  deconvolution (precomputed_function<luminosity_t> *mtf);
+  deconvolution (precomputed_function<luminosity_t> *mtf, int max_threads, bool sharpen = true);
   typedef double deconvolution_data_t;
-  ~deconvolution () { delete m_blur_kernel; }
+  ~deconvolution ();
 
-  void blur_tile (deconvolution_data_t *d);
-  void sharpen_tile (deconvolution_data_t *d);
+  void process_tile (int thread_id);
   int
   get_basic_tile_size () const
   {
@@ -35,11 +35,32 @@ public:
     return m_border_size;
   }
 
+  void init (int thread_id);
+
+  void
+  put_pixel (int threadid, int x, int y, deconvolution_data_t val)
+  {
+    m_plans[threadid].tile[y * m_tile_size + x] = val;
+  }
+  deconvolution_data_t
+  get_pixel (int threadid, int x, int y)
+  {
+    return m_plans[threadid].tile[y * m_tile_size + x];
+  }
+
 private:
   int m_border_size;
   int m_tile_size;
   int m_fft_size;
   fftw_complex *m_blur_kernel;
+  struct fftw_plans
+  {
+    fftw_plan plan_2d_inv, plan_2d;
+    fftw_complex *in;
+    std::vector<deconvolution_data_t> tile;
+    bool initialized;
+  };
+  std::vector <fftw_plans> m_plans;
 };
 
 /* Deconvolution worker. Sharpen DATA to OUT which both has dimensions
@@ -54,22 +75,22 @@ deconvolute (mem_O *out, T data, P param, int width, int height,
              precomputed_function<luminosity_t> *mtf, progress_info *progress,
              bool parallel = true)
 {
-  deconvolution d (mtf);
+  int nthreads = parallel ? omp_get_max_threads () : 1;
+  deconvolution d (mtf, nthreads);
 
   int xtiles
       = (width + d.get_basic_tile_size () - 1) / d.get_basic_tile_size ();
   int ytiles
       = (height + d.get_basic_tile_size () - 1) / d.get_basic_tile_size ();
-  std::vector<deconvolution::deconvolution_data_t> tile (
-      d.get_tile_size_with_borders () * d.get_tile_size_with_borders ());
   if (progress)
     progress->set_task ("Deconvolution sharpening", xtiles * ytiles);
-//#pragma omp parallel for shared(progress,out,width, height, param, data,d,tile) collapse(2) default(none) if (parallel)
   for (int y = 0; y < height; y += d.get_basic_tile_size ())
     for (int x = 0; x < width; x += d.get_basic_tile_size ())
       {
         if (progress && progress->cancelled ())
 	  continue;
+	int id = parallel ? omp_get_thread_num (): 0;
+	d.init (id);
 	//printf ("%i %i\n",x,y);
         for (int yy = 0; yy < d.get_tile_size_with_borders (); yy++)
           for (int xx = 0; xx < d.get_tile_size_with_borders (); xx++)
@@ -81,16 +102,14 @@ deconvolute (mem_O *out, T data, P param, int width, int height,
                   && y + yy - d.get_border_size () < height)
                 pixel = getdata (data, x + xx - d.get_border_size (),
                                  y + yy - d.get_border_size (), width, param);
-              tile[yy * d.get_tile_size_with_borders () + xx] = pixel;
+              d.put_pixel(id, xx, yy, pixel);
             }
-        d.sharpen_tile (tile.data ());
+        d.process_tile (id);
         for (int yy = 0; yy < d.get_basic_tile_size (); yy++)
           for (int xx = 0; xx < d.get_basic_tile_size (); xx++)
 	    if (y + yy < height && x + xx < width)
 	      out[(y + yy) * width + x + xx]
-		  = tile[(yy + d.get_border_size ())
-			     * d.get_tile_size_with_borders ()
-			 + xx + d.get_border_size ()];
+		  = d.get_pixel (id, xx + d.get_border_size (), yy + d.get_border_size ());
         if (progress)
           progress->inc_progress ();
       }
