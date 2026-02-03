79ab7a5c36ddab5c759c09dea3f47813306af45a
Jan Hubicka <hubicka@ucw.cz>
2026-02-03 12:19:15 +0100

    Refactor analyze_scanner_blur to libcolorscreen

--- src/colorscreen/colorscreen.C
+++ src/colorscreen/colorscreen.C
@@ -11,6 +11,7 @@
 #endif
 
 #include "../libcolorscreen/include/colorscreen.h"
+#include "../libcolorscreen/include/analyze-scanner-blur.h"
 #include "../libcolorscreen/include/dufaycolor.h"
 #include "../libcolorscreen/include/finetune.h"
 #include "../libcolorscreen/include/histogram.h"
@@ -1272,271 +1273,36 @@ analyze_scanner_blur_img (scr_to_img_parameters &param,
 			  coord_t tolerance,
 			  progress_info *progress)
 {
-  scanner_blur_correction_parameters::correction_mode mode = scanner_blur_correction_parameters::blur_radius;
-  if (flags & (finetune_scanner_mtf_defocus | finetune_scanner_mtf_channel_defocus))
-    mode = rparam.sharpen.scanner_mtf.simulate_difraction_p ()
-	   ? scanner_blur_correction_parameters::mtf_defocus : scanner_blur_correction_parameters::mtf_blur_diameter;
-  {
-    std::vector<finetune_result> prepass (strip_xsteps * strip_ysteps);
-    if (verbose)
-      {
-	progress->pause_stdout ();
-	if (screen_with_varying_strips_p (param.type))
-	  printf ("Analyzing %ix%i areas to determine strip widths and "
-		  "blur (overall %i solutions to be computed)\n",
-		  strip_xsteps, strip_ysteps, strip_xsteps * strip_ysteps);
-	else
-	  printf ("Analyzing %ix%i areas to determine blur (overall %i "
-		  "solutions to be computed)\n",
-		  strip_xsteps, strip_ysteps, strip_xsteps * strip_ysteps);
-	progress->resume_stdout ();
-      }
-    progress->set_task (screen_with_varying_strips_p (param.type) ? "analyzing screen strip sizes and blur"
-			      : "analyzing screen blur",
-		       strip_xsteps * strip_ysteps);
-#pragma omp parallel for default(none) collapse(2) schedule(dynamic)          \
-      shared(strip_xsteps, strip_ysteps, rparam, scan, progress, param, prepass, \
-		 flags)
-    for (int y = 0; y < strip_ysteps; y++)
-      for (int x = 0; x < strip_xsteps; x++)
-	{
-	  finetune_parameters fparam;
-	  fparam.flags = flags | (screen_with_varying_strips_p (param.type) ? finetune_strips : 0);
-	  fparam.multitile = 1;
-	  prepass [y * strip_xsteps + x] = finetune (
-	      rparam, param, scan,
-	      { { (coord_t)(x + 0.5) * scan.width / strip_xsteps,
-		  (coord_t)(y + 0.5) * scan.height / strip_ysteps } },
-	      NULL, fparam, progress);
-	  progress->inc_progress ();
-	}
-    histogram uncertainity_hist;
-    histogram red_hist;
-    histogram green_hist;
-    histogram blur_hist;
-    int nok = 0;
-    for (int y = 0; y < strip_ysteps; y++)
-      for (int x = 0; x < strip_xsteps; x++)
-	{
-	  finetune_result &res = prepass[y * strip_xsteps + x];
-	  if (res.success)
-	    uncertainity_hist.pre_account (res.uncertainity);
-	}
-    uncertainity_hist.finalize_range (65536);
-    for (int y = 0; y < strip_ysteps; y++)
-      for (int x = 0; x < strip_xsteps; x++)
-	{
-	  finetune_result &res = prepass[y * strip_xsteps + x];
-	  if (res.success)
-	    uncertainity_hist.account (res.uncertainity);
-	}
-    uncertainity_hist.finalize ();
-    coord_t uncertainity_threshold = uncertainity_hist.find_max (skipmax / 100.0);
-    for (int y = 0; y < strip_ysteps; y++)
-      for (int x = 0; x < strip_xsteps; x++)
-	{
-	  finetune_result &res = prepass[y * strip_xsteps + x];
-	  if (!res.success || res.uncertainity > uncertainity_threshold)
-	    continue;
-	  if (screen_with_varying_strips_p (param.type))
-	    {
-	      red_hist.pre_account (res.red_strip_width);
-	      green_hist.pre_account (res.green_strip_width);
-	    }
-	  blur_hist.pre_account (get_correction (mode, res));
-	  nok++;
-	}
-    if (!nok)
-      {
-	progress->pause_stdout ();
-	fprintf (stderr, "Analysis failed\n");
-	return NULL;
-      }
-    if (screen_with_varying_strips_p (param.type))
-      {
-	red_hist.finalize_range (65536);
-	green_hist.finalize_range (65536);
-      }
-    blur_hist.finalize_range (65536);
-    for (int y = 0; y < strip_ysteps; y++)
-      for (int x = 0; x < strip_xsteps; x++)
-	{
-	  finetune_result &res = prepass[y * strip_xsteps + x];
-	  if (!res.success || res.uncertainity > uncertainity_threshold)
-	    continue;
-	  if (screen_with_varying_strips_p (param.type))
-	    {
-	      red_hist.account (res.red_strip_width);
-	      green_hist.account (res.green_strip_width);
-	    }
-	  blur_hist.account (get_correction (mode, res));
-	  nok++;
-	}
-
-    if (screen_with_varying_strips_p (param.type))
-      {
-	red_hist.finalize ();
-	green_hist.finalize ();
-      }
-    blur_hist.finalize ();
-    if (screen_with_varying_strips_p (param.type))
-      {
-	rparam.red_strip_width
-	    = red_hist.find_avg (skipmin / 100, skipmax / 100);
-	rparam.green_strip_width
-	    = green_hist.find_avg (skipmin / 100, skipmax / 100);
-      }
-    switch (mode)
-      {
-      case scanner_blur_correction_parameters::blur_radius:
-	rparam.screen_blur_radius
-	    = blur_hist.find_avg (skipmin / 100, skipmax / 100);
-	break;
-      case scanner_blur_correction_parameters::mtf_defocus:
-	rparam.sharpen.scanner_mtf.defocus
-	    = blur_hist.find_avg (skipmin / 100, skipmax / 100);
-	break;
-      case scanner_blur_correction_parameters::mtf_blur_diameter:
-	rparam.sharpen.scanner_mtf.blur_diameter
-	    = blur_hist.find_avg (skipmin / 100, skipmax / 100);
-	break;
-      case scanner_blur_correction_parameters::max_correction:
-	abort ();
-      }
-    if (verbose)
-      {
-	progress->pause_stdout ();
-	if (screen_with_varying_strips_p (param.type))
-	  {
-	    printf ("Red strip width %.2f%%\n",
-		    rparam.red_strip_width * 100);
-	    printf ("Green strip width %.2f%%\n",
-		    rparam.green_strip_width * 100);
-	  }
-	switch (mode)
-	  {
-	  case scanner_blur_correction_parameters::blur_radius:
-	    printf ("Average screen blur %.2f pixels\n", rparam.screen_blur_radius);
-	    break;
-	  case scanner_blur_correction_parameters::mtf_defocus:
-	    printf ("Average mtf defocus %.5f mm\n", rparam.sharpen.scanner_mtf.defocus);
-	    break;
-	  case scanner_blur_correction_parameters::mtf_blur_diameter:
-	    printf ("Average mtf blur diameter %.2f pixels\n", rparam.sharpen.scanner_mtf.blur_diameter);
-	    break;
-	  case scanner_blur_correction_parameters::max_correction:
-	    abort ();
-	  }
-	progress->resume_stdout ();
-      }
-  }
-  if (verbose)
-    {
-      progress->pause_stdout ();
-      printf ("Analyzing %ix%i areas each subsampled %ix%i (overall %i "
-              "solutions to be computed)\n",
-              xsteps, ysteps, xsubsteps, ysubsteps,
-              xsteps * ysteps * xsubsteps * ysubsteps);
-      progress->resume_stdout ();
-    }
-  std::vector<finetune_result> mainpass (xsteps * xsubsteps * ysteps * ysubsteps);
-  progress->set_task ("analyzing samples",
-                      ysteps * xsteps * xsubsteps * ysubsteps);
-#pragma omp parallel for default(none) collapse(2) schedule(dynamic)          \
-    shared(xsteps, ysteps, xsubsteps, ysubsteps, rparam, scan, progress,      \
-               param, mainpass, reoptimize_strip_widths, flags)
-  for (int y = 0; y < ysteps * ysubsteps; y++)
-    for (int x = 0; x < xsteps * xsubsteps; x++)
-      {
-        finetune_parameters fparam;
-        fparam.flags = flags | (reoptimize_strip_widths ? finetune_strips : 0);
-        fparam.multitile = 1;
-        mainpass[y * xsteps * xsubsteps + x] = finetune (
-            rparam, param, scan,
-            { { (coord_t)(x + 0.5) * scan.width / (xsteps * xsubsteps),
-                (coord_t)(y + 0.5) * scan.height / (ysteps * ysubsteps) } },
-            NULL, fparam, progress);
-        progress->inc_progress ();
-      }
-  std::unique_ptr <scanner_blur_correction_parameters> scanner_blur_correction = std::make_unique<scanner_blur_correction_parameters> ();
-  scanner_blur_correction->alloc (xsteps, ysteps, mode);
-  coord_t pixel_size;
-  scr_to_img map;
-  map.set_parameters (param, scan);
-  pixel_size = map.pixel_size (scan.width, scan.height);
-  progress->set_task ("summarizing results", 1);
-  bool fail = false;
-  for (int y = 0; y < ysteps; y++)
-    for (int x = 0; x < xsteps; x++)
-      {
-        int nok = 0;
-        histogram uncertainity_hist;
-        for (int yy = 0; yy < ysubsteps; yy++)
-          for (int xx = 0; xx < xsubsteps; xx++)
-	    {
-	      finetune_result &res = mainpass[(y * ysubsteps + yy) * xsteps * xsubsteps + x * xsubsteps + xx];
-	      if (res.success)
-		uncertainity_hist.pre_account (res.uncertainity);
-	    }
-	uncertainity_hist.finalize_range (65536);
-        for (int yy = 0; yy < ysubsteps; yy++)
-          for (int xx = 0; xx < xsubsteps; xx++)
-	    {
-	      finetune_result &res = mainpass[(y * ysubsteps + yy) * xsteps * xsubsteps + x * xsubsteps + xx];
-	      if (res.success)
-		uncertainity_hist.account (res.uncertainity);
-	    }
-        uncertainity_hist.finalize ();
-        coord_t uncertainity_threshold = uncertainity_hist.find_max (skipmax / 100.0);
-        histogram hist;
-        for (int yy = 0; yy < ysubsteps; yy++)
-          for (int xx = 0; xx < xsubsteps; xx++)
-            {
-	      finetune_result &res = mainpass[(y * ysubsteps + yy) * xsteps * xsubsteps + x * xsubsteps + xx];
-	      if (!res.success || res.uncertainity > uncertainity_threshold)
-		continue;
-	      nok++;
-	      hist.pre_account (get_correction (mode, res));
-            }
-        if (!nok)
-          {
-            progress->pause_stdout ();
-            fprintf (stderr, "Analysis failed for sample %i,%i\n", x, y);
-            return NULL;
-          }
-        hist.finalize_range (65536);
-        for (int yy = 0; yy < ysubsteps; yy++)
-          for (int xx = 0; xx < xsubsteps; xx++)
-            {
-	      finetune_result &res = mainpass[(y * ysubsteps + yy) * xsteps * xsubsteps + x * xsubsteps + xx];
-              hist.account (get_correction (mode, res));
-            }
-        hist.finalize ();
-        if (tolerance >= 0
-            && hist.find_max (skipmax / 100.0) - hist.find_min (skipmin / 100)
-                   > tolerance)
-          {
-	    progress->pause_stdout ();
-            printf ("Tolerance threshold %f exceeded for entry %i,%i: %s "
-                    " range is %f...%f (diff %f)\n",
-                    tolerance, x, y, 
-		    scanner_blur_correction_parameters::pretty_correction_names[(int)mode],
-		    hist.find_min (skipmin / 100),
-                    hist.find_max (skipmax / 100.0),
-                    hist.find_max (skipmax / 100.0)
-                        - hist.find_min (skipmin / 100));
-            fail = true;
-	    progress->resume_stdout ();
-          }
-        luminosity_t b = hist.find_avg (skipmin / 100, skipmax / 100);
-        assert (b >= 0 && b <= 1024);
-	if (mode == scanner_blur_correction_parameters::blur_radius)
-	  b *= pixel_size;
-        scanner_blur_correction->set_correction (x, y, b);
-      }
-  if (fail)
+  analyze_scanner_blur_worker worker (param, rparam, scan);
+  worker.strip_xsteps = strip_xsteps;
+  worker.strip_ysteps = strip_ysteps;
+  worker.xsteps = xsteps;
+  worker.ysteps = ysteps;
+  worker.xsubsteps = xsubsteps;
+  worker.ysubsteps = ysubsteps;
+  worker.flags = flags;
+  worker.reoptimize_strip_widths = reoptimize_strip_widths;
+  worker.skipmin = skipmin;
+  worker.skipmax = skipmax;
+  worker.tolerance = tolerance;
+  worker.progress = progress;
+  worker.verbose = verbose;
+  if (!worker.step1 ())
     return NULL;
-  return scanner_blur_correction;
+  if (worker.do_strips ())
+    {
+#pragma omp parallel for default(none) collapse(2) schedule(dynamic) shared (worker)
+      for (int y = 0; y < worker.strip_ysteps; y++)
+	for (int x = 0; x < worker.strip_xsteps; x++)
+	  worker.analyze_strips (x, y);
+    }
+  if (!worker.step2 ())
+    return NULL;
+#pragma omp parallel for default(none) collapse(2) schedule(dynamic) shared (worker)
+  for (int y = 0; y < worker.ysteps * worker.ysubsteps; y++)
+    for (int x = 0; x < worker.xsteps * worker.xsubsteps; x++)
+      worker.analyze_blur (x, y);
+  return worker.step3 ();
 }
 
 static bool
---
+++ src/libcolorscreen/include/analyze-scanner-blur.h
@@ -0,0 +1,50 @@
+#ifndef ANALYZE_SCANNER_BLUR_H
+#define ANALYZE_SCANNER_BLUR_H
+#include "colorscreen.h"
+#include "scanner-blur-correction-parameters.h"
+#include "histogram.h"
+#include "scr-to-img-parameters.h"
+#include "imagedata.h"
+#include "finetune.h"
+namespace colorscreen
+{
+class
+analyze_scanner_blur_worker
+{
+  public:
+  analyze_scanner_blur_worker (scr_to_img_parameters &param1, render_parameters &rparam1, image_data &scan1)
+  : param (param1), rparam (rparam1), scan (scan1), strip_xsteps (0), strip_ysteps (0), xsteps (0), ysteps (0), xsubsteps (0), ysubsteps (0), flags (finetune_position | finetune_no_progress_report | finetune_scanner_mtf_defocus), reoptimize_strip_widths (false), skipmin (25), skipmax (25), tolerance (-1), progress (NULL), verbose (false)
+  { }
+  scr_to_img_parameters &param;
+  render_parameters &rparam;
+  image_data &scan;
+  int strip_xsteps, strip_ysteps;
+  int xsteps, ysteps;
+  int xsubsteps, ysubsteps;
+  uint64_t flags;
+  bool reoptimize_strip_widths;
+  coord_t skipmin; coord_t skipmax;
+  coord_t tolerance;
+  progress_info *progress;
+  bool verbose;
+
+  DLL_PUBLIC bool step1 ();
+  bool do_strips ()
+  {
+    return prepass.size () > 0;
+  }
+  DLL_PUBLIC bool analyze_strips (int x, int y);
+  DLL_PUBLIC bool step2 ();
+  DLL_PUBLIC bool analyze_blur (int x, int y);
+  DLL_PUBLIC std::unique_ptr <scanner_blur_correction_parameters> step3 ();
+private:
+  scanner_blur_correction_parameters::correction_mode mode;
+  histogram uncertainity_hist;
+  histogram red_hist;
+  histogram green_hist;
+  histogram blur_hist;
+  std::vector<finetune_result> prepass;
+  std::vector<finetune_result> mainpass;
+};
+}
+#endif
