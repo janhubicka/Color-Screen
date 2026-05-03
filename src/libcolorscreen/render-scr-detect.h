b782ed204c6a6356f18d52d1f653c162c4364c2c
Jan Hubicka <hubicka@ucw.cz>
2026-05-03 14:41:56 +0200

    scr-detect comments.

--- src/libcolorscreen/render-scr-detect.h
+++ src/libcolorscreen/render-scr-detect.h
@@ -1,3 +1,7 @@
+/* Rendering logic for screen detection.
+   Copyright (C) 2014-2026 Jan Hubicka
+   This file is part of Color-Screen.  */
+
 #ifndef RENDER_SCR_DETECT_H
 #define RENDER_SCR_DETECT_H
 #include "include/progress-info.h"
@@ -8,15 +12,21 @@
 
 namespace colorscreen
 {
-
+/* Parameters for color classification cache.  */
 struct color_class_params
 {
-  uint64_t image_id;
-  const image_data *img;
-  rgbdata *precomputed_rgbdata;
+  /* ID of input image.  */
+  uint64_t image_id = 0;
+  /* Input image data.  */
+  const image_data *img = nullptr;
+  /* Precomputed RGB data (optional).  */
+  rgbdata *precomputed_rgbdata = nullptr;
+  /* Screen detection parameters.  */
   scr_detect_parameters p;
-  class scr_detect *d;
-  luminosity_t gamma;
+  /* Screen detection logic.  */
+  class scr_detect *d = nullptr;
+  /* Gamma value for linearization.  */
+  luminosity_t gamma = 0;
 
   bool
   operator==(const color_class_params &o) const
@@ -28,15 +38,22 @@ struct color_class_params
 };
 std::unique_ptr<color_class_map> get_color_class_map(color_class_params &, progress_info *);
 
+/* Parameters for precomputed RGB data cache.  */
 struct precomputed_rgbdata_params
 {
-  uint64_t image_id;
+  /* ID of input image.  */
+  uint64_t image_id = 0;
+  /* Screen detection parameters.  */
   scr_detect_parameters p;
-  luminosity_t gamma;
+  /* Gamma value for linearization.  */
+  luminosity_t gamma = 0;
 
-  const image_data *img;
-  class scr_detect *d;
-  class render_scr_detect *r;
+  /* Input image data.  */
+  const image_data *img = nullptr;
+  /* Screen detection logic.  */
+  class scr_detect *d = nullptr;
+  /* Renderer used for precomputation.  */
+  class render_scr_detect *r = nullptr;
 
   bool
   operator==(const precomputed_rgbdata_params &o) const
@@ -54,13 +71,19 @@ struct precomputed_rgbdata_params
 struct precomputed_rgbdata;
 std::unique_ptr<precomputed_rgbdata> get_precomputed_rgbdata(precomputed_rgbdata_params &, progress_info *);
 
+/* Parameters for patches cache.  */
 struct patches_cache_params
 {
-  uint64_t scr_map_id;
-  uint64_t gray_data_id;
-  class color_class_map *map;
-  const image_data *img;
-  class render *r;
+  /* ID of color class map.  */
+  uint64_t scr_map_id = 0;
+  /* ID of grayscale data.  */
+  uint64_t gray_data_id = 0;
+  /* Color class map.  */
+  class color_class_map *map = nullptr;
+  /* Input image data.  */
+  const image_data *img = nullptr;
+  /* Renderer used for patch detection.  */
+  class render *r = nullptr;
 
   /* TODO: render parameters affects luminosity.  */
   bool
@@ -72,13 +95,19 @@ struct patches_cache_params
 };
 std::unique_ptr<patches> get_patches(patches_cache_params &, progress_info *);
 
+/* Parameters for color data cache.  */
 struct color_data_params
 {
-  uint64_t color_class_map_id;
-  uint64_t graydata_id;
-  const image_data *img;
-  class color_class_map *map;
-  class render *r;
+  /* ID of color class map.  */
+  uint64_t color_class_map_id = 0;
+  /* ID of grayscale data.  */
+  uint64_t graydata_id = 0;
+  /* Input image data.  */
+  const image_data *img = nullptr;
+  /* Color class map.  */
+  class color_class_map *map = nullptr;
+  /* Renderer used for color data computation.  */
+  class render *r = nullptr;
 
 
   bool
@@ -96,12 +125,18 @@ typedef lru_cache<patches_cache_params, patches, get_patches, 4> patches_cache_t
 typedef lru_cache<color_data_params, struct color_data, get_new_color_data, 10> color_data_cache_t;
 
 struct render_to_file_params;
+/* Renderer using screen detection logic.  */
 class render_scr_detect : public render
 {
 public:
+  /* Initialize screen detection renderer.
+     PARAM specifies screen detection parameters.
+     IMG is the input scan data.
+     RPARAM specifies rendering parameters.
+     DSTMAXVAL is the maximum destination pixel value.  */
   render_scr_detect (const scr_detect_parameters &param, const image_data &img, const render_parameters &rparam, int dstmaxval)
     : render (img, rparam, dstmaxval),
-      m_precomputed_rgbdata (NULL), m_precomputed_rgbdata_holder (),
+      m_precomputed_rgbdata (nullptr), m_precomputed_rgbdata_holder (),
       m_color_class_map ()
   {
     /* TODO: Move to precomputation and also check return value, pass progress.  */
@@ -111,6 +146,8 @@ public:
   void set_render_type (render_type_parameters)
   {
   }
+  /* Classify pixel at position P.
+     Return the color class of the pixel or unknown if it is not a screen pixel.  */
   scr_detect::color_class classify_pixel (int_point_t p)
   {
     if (p.x < 0 || p.x >= m_img.width || p.y < 0 || p.y >= m_img.height)
@@ -128,6 +165,7 @@ public:
 	  }
     return t;
   }
+  /* Get the color of the screen at position P.  */
   pure_attr inline rgbdata
   get_screen_color (point_t p) const
   {
@@ -165,8 +203,15 @@ public:
   {
     abort ();
   }
+  /* Precompute all data needed for rendering.
+     GRAYSCALE_NEEDED specifies if grayscale data is needed.
+     NORMALIZED_PATCHES specifies if patches should be normalized.
+     PROGRESS is used to report progress and check for cancellation.  */
   bool precompute_all (bool grayscale_needed, bool normalized_patches, progress_info *progress);
+  /* Precompute adjusted RGB data.
+     PROGRESS is used to report progress and check for cancellation.  */
   bool precompute_rgbdata (progress_info *progress);
+  /* Get the adjusted pixel value at position P.  */
   pure_attr inline rgbdata get_adjusted_pixel (point_t p) const
   {
     coord_t xp = p.x - (coord_t)0.5;
@@ -207,6 +252,7 @@ public:
     else
       return {0, 0, 0};
   }
+  /* Normalize color C so that the sum of its components is 1.  */
   static rgbdata
   normalize_color (rgbdata c)
   {
@@ -234,20 +280,24 @@ public:
   }
   /* TODO: 16bit floats seems to be too slow for screen detection.  */
   typedef rgbdata my_mem_rgbdata;
+  /* Get precomputed adjusted pixel at integer position P.  */
   pure_attr inline my_mem_rgbdata fast_precomputed_get_adjusted_pixel (int_point_t p) const
   {
     return m_precomputed_rgbdata[p.y * m_img.width + p.x];
   }
+  /* Get precomputed normalized pixel at integer position P.  */
   pure_attr inline my_mem_rgbdata fast_precomputed_get_normalized_pixel (int_point_t p) const
   {
     return normalize_color (m_precomputed_rgbdata[p.y * m_img.width + p.x]);
   }
+  /* Adjust linearized color C.  */
   pure_attr inline rgbdata adjust_linearized_color (rgbdata c) const
   {
     rgbdata d;
     m_scr_detect.adjust_linearized_color (c.red, c.green, c.blue, &d.red, &d.green, &d.blue);
     return d;
   }
+  /* Get non-precomputed adjusted pixel at integer position P.  */
   pure_attr inline rgbdata fast_nonprecomputed_get_adjusted_pixel (int_point_t p) const
   {
     rgbdata d;
@@ -255,6 +305,7 @@ public:
     m_scr_detect.adjust_color (p_pixel.r, p_pixel.g, p_pixel.b, &d.red, &d.green, &d.blue);
     return d;
   }
+  /* Get non-precomputed normalized pixel at integer position P.  */
   pure_attr inline rgbdata fast_nonprecomputed_get_normalized_pixel (int_point_t p) const
   {
     rgbdata d;
@@ -262,6 +313,7 @@ public:
     m_scr_detect.adjust_color (p_pixel.r, p_pixel.g, p_pixel.b, &d.red, &d.green, &d.blue);
     return normalize_color (d);
   }
+  /* Get adjusted pixel at integer position P.  */
   pure_attr inline rgbdata fast_get_adjusted_pixel (int_point_t p) const
   {
     if (m_precomputed_rgbdata)
@@ -269,11 +321,13 @@ public:
     else
       return fast_nonprecomputed_get_adjusted_pixel (p);
   }
+  /* Get normalized pixel at integer position P.  */
   pure_attr inline rgbdata fast_get_normalized_pixel (int_point_t p) const
   {
     rgbdata c = normalize_color (fast_get_adjusted_pixel (p));
     return c;
   }
+  /* Get screen color at integer position P.  */
   pure_attr inline rgbdata fast_get_screen_pixel (int_point_t p) const
   {
     rgbdata d;
@@ -354,28 +408,60 @@ done:
       }
      return val / end;
   }
+  /* Render a tile of the image.
+     RTPARAM specifies the type of rendering.
+     PARAM specifies screen detection parameters.
+     IMG is the input scan.
+     RPARAM specifies rendering parameters.
+     PIXELS is the destination buffer.
+     ROWSTRIDE is the number of bytes per row in the destination buffer.
+     PIXELBYTES is the number of bytes per pixel in the destination buffer.
+     WIDTH, HEIGHT specifies the size of the tile.
+     XOFFSET, YOFFSET specifies the offset of the tile in scan coordinates.
+     STEP specifies the size of a single pixel in scan coordinates.
+     P is used to report progress and check for cancellation.  */
   static bool render_tile (render_type_parameters &rtparam, scr_detect_parameters &param, image_data &img, render_parameters &rparam,
 			   unsigned char *pixels, int rowstride, int pixelbytes, int width, int height,
-			   double xoffset, double yoffset, double step, progress_info *p = NULL);
+			   double xoffset, double yoffset, double step, progress_info *p = nullptr);
+  /* Render the image to a file.
+     RFPARAMS specifies the output file parameters.
+     RTPARAM specifies the type of rendering.
+     PARAM specifies screen geometry mapping.
+     DPARAM specifies screen detection parameters.
+     RPARAM specifies rendering parameters.
+     IMG is the input scan data.
+     BLACK is the black level.
+     PROGRESS is used to report progress and check for cancellation.  */
   static const char *render_to_file (render_to_file_params &rfparams, render_type_parameters rtparam, scr_to_img_parameters &param, scr_detect_parameters &dparam, render_parameters rparam, image_data &img, int black, progress_info *progress);
-  color_class_map *get_color_class_map ()
-  {
-    return m_color_class_map.get ();
-  }
+  /* Analyze color proportions in a given range.
+     PARAM specifies screen geometry mapping.
+     XMIN, YMIN, XMAX, YMAX specifies the range in scan coordinates.
+     P is used to report progress and check for cancellation.  */
   rgbdata analyze_color_proportions (scr_to_img_parameters *param, int xmin, int ymin, int xmax, int ymax, progress_info *p);
 protected:
-  my_mem_rgbdata *m_precomputed_rgbdata;
+  /* Precomputed RGB data.  */
+  my_mem_rgbdata *m_precomputed_rgbdata = nullptr;
+  /* Holder for precomputed RGB data.  */
   std::shared_ptr<precomputed_rgbdata> m_precomputed_rgbdata_holder;
+  /* Map of color classes for each pixel.  */
   std::shared_ptr<color_class_map> m_color_class_map;
+  /* Screen detection parameters and state.  */
   scr_detect m_scr_detect;
-  uint64_t m_color_class_map_id;
-  uint64_t m_precomputed_rgbdata_id;
+  /* ID of the color class map.  */
+  uint64_t m_color_class_map_id = 0;
+  /* ID of the precomputed RGB data.  */
+  uint64_t m_precomputed_rgbdata_id = 0;
 };
 
-/* Simple wrapper to be used by rendering templates.  */
+/* Simple wrapper to be used by rendering templates to render adjusted colors.  */
 class render_scr_detect_adjusted : public render_scr_detect
 {
 public:
+  /* Initialize adjusted color renderer.
+     PARAM specifies screen detection parameters.
+     IMG is the input scan data.
+     RPARAM specifies rendering parameters.
+     DSTMAXVAL is the maximum destination pixel value.  */
   render_scr_detect_adjusted (scr_detect_parameters &param, image_data &img, render_parameters &rparam, int dstmaxval)
   : render_scr_detect (param, img, rparam, dstmaxval)
   { }
@@ -411,10 +497,15 @@ public:
   }
 };
 
-/* Simple wrapper to be used by rendering templates.  */
+/* Simple wrapper to be used by rendering templates to render normalized colors.  */
 class render_scr_detect_normalized : public render_scr_detect
 {
 public:
+  /* Initialize normalized color renderer.
+     PARAM specifies screen detection parameters.
+     IMG is the input scan data.
+     RPARAM specifies rendering parameters.
+     DSTMAXVAL is the maximum destination pixel value.  */
   render_scr_detect_normalized (scr_detect_parameters &param, image_data &img, render_parameters &rparam, int dstmaxval)
   : render_scr_detect (param, img, rparam, dstmaxval)
   { }
@@ -442,10 +533,15 @@ public:
   }
   bool get_color_data (rgbdata *data, point_t p, int width, int height, coord_t pixelsize, progress_info *progress);
 };
-/* Simple wrapper to be used by rendering templates.  */
+/* Simple wrapper to be used by rendering templates to render pixel colors.  */
 class render_scr_detect_pixel_color : public render_scr_detect
 {
 public:
+  /* Initialize pixel color renderer.
+     PARAM specifies screen detection parameters.
+     IMG is the input scan data.
+     RPARAM specifies rendering parameters.
+     DSTMAXVAL is the maximum destination pixel value.  */
   render_scr_detect_pixel_color (scr_detect_parameters &param, image_data &img, render_parameters &rparam, int dstmaxval)
   : render_scr_detect (param, img, rparam, dstmaxval)
   { }
@@ -550,7 +646,7 @@ class render_scr_relax : public render_scr_detect
 {
 public:
   inline render_scr_relax (scr_detect_parameters &param, image_data &data, render_parameters &rparam, int dst_maxval)
-   : render_scr_detect (param, data, rparam, dst_maxval), m_color_data_handle (NULL)
+   : render_scr_detect (param, data, rparam, dst_maxval), m_color_data_handle (nullptr)
   { 
   }
   bool precompute_all (progress_info *progress);
@@ -599,9 +695,15 @@ private:
     }
   std::shared_ptr<color_data> m_color_data_handle;
 };
+/* Nearest neighbor screen color renderer.  */
 class render_scr_nearest : public render_scr_detect
 {
 public:
+  /* Initialize nearest neighbor renderer.
+     PARAM specifies screen detection parameters.
+     DATA is the input scan data.
+     RPARAM specifies rendering parameters.
+     DST_MAXVAL is the maximum destination pixel value.  */
   inline render_scr_nearest (scr_detect_parameters &param, image_data &data, render_parameters &rparam, int dst_maxval)
    : render_scr_detect (param, data, rparam, dst_maxval)
   { 
@@ -674,9 +776,15 @@ private:
 };
 
 extern class distance_list distance_list;
+/* Scaled nearest neighbor screen color renderer.  */
 class render_scr_nearest_scaled : public render_scr_detect
 {
 public:
+  /* Initialize scaled nearest neighbor renderer.
+     PARAM specifies screen detection parameters.
+     DATA is the input scan data.
+     RPARAM specifies rendering parameters.
+     DST_MAXVAL is the maximum destination pixel value.  */
   inline render_scr_nearest_scaled (scr_detect_parameters &param, image_data &data, render_parameters &rparam, int dst_maxval)
    : render_scr_detect (param, data, rparam, dst_maxval), m_patches ()
   { 
